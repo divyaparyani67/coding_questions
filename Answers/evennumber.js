f8991309eece1b6fca8554211ca215fe38ace239
divyaparyani67 <divyaparyani67@gmail.com>
2023-05-02 21:30:21 +0530

    answers added

--- Answers/evennumber.js
+++ Answers/evennumber.js
@@ -1,8 +1,12 @@
 //se the for loop to output even numbers from 2 to 10.
 
-
-for (let i = 2; i <= 10; i++) {
-  if (i % 2 == 0) {
-    alert( i );
+function getevenNumber(n) {
+  for (let i = n; i <= 10; i++) {
+    if (i % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
+console.log(getevenNumber(9));
