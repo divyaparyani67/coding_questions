3671c599cd9f56bf543b07f20a9da21776cab1b5
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-05-04 07:56:48 +0530

    Create containsduplicate.js

---
+++ Answers/containsduplicate.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function(nums) {
+ for(let i = 0; i < nums.length; i++){
+    for(let j =0; j < i; j++){
+          if (nums[i] === nums[j]){
+             return  true;            
+          }
+       }
+    }
+    return false;
+ }
