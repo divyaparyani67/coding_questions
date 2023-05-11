7356d724277eaba03c235f240d84520cee050f24
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-05-11 08:13:25 +0530

    Update 2sum.js

--- Answers/2sum.js
+++ Answers/2sum.js
@@ -1,10 +1,13 @@
 var twoSum = function(nums, target) {
 
-   for (let i = 0; i < nums.length; i++){
-       for (let j = 1; j < nums.length; i++) {
-           if ( nums[i] + [j] === target){
-               return [i,j]
-           }
+    var hash = {}
+    var len = nums.length;
+
+   for (var i = 0; i < nums.length; i++){
+           if ( nums[i] in hash)
+           return [hash[nums[i]], i] 
+           hash[target -nums[i]] = i 
        }
-   }
+       return [-1, -1]
+   
 };
