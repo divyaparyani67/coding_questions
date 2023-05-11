1f534db5f1c008f31263b0115938c32b40f817f2
Divya Paryani <106226581+divyaparyani67@users.noreply.github.com>
2023-05-11 08:25:34 +0530

    Update containsduplicate.js

--- Answers/containsduplicate.js
+++ Answers/containsduplicate.js
@@ -1,3 +1,26 @@
+
+//from a sorted arrauy
+var removeDuplicates = function(nums) {
+
+    var len = nums.length
+    var last = NaN
+    var count = 0
+
+    for( var i = 0; i < nums.length; i++)
+   {
+        if(nums[i] !== last) {
+            nums[count] = nums[i];
+            last = nums[i];
+            count++
+        }
+   }
+   return count;
+    
+};
+
+
+**********************
+
 var containsDuplicate = function(nums) {
  for(let i = 0; i < nums.length; i++){
     for(let j =0; j < i; j++){
